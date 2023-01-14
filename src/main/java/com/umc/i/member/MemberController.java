98a1a4e6b64c8a9677d13544b671cbb8869dbaac
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2023-01-15 00:03:23 +0900

    Refactor: 시스템 로그 삭제, ResponseEntity 변경

--- src/main/java/com/umc/i/member/MemberController.java
+++ src/main/java/com/umc/i/member/MemberController.java
@@ -2,10 +2,15 @@ package com.umc.i.member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Slf4j
@@ -17,15 +22,18 @@ public class MemberController {
 
     @ResponseBody
     @RequestMapping("/member/login")
-    public String login(@RequestBody Member member) {
-        log.info("requestEmail={}", member.getEmail());
+    public ResponseEntity<Map<String, Object>> login(@RequestBody Member member) {
         Member loginMember = memberService.login(member.getEmail(), member.getPassword());
 
-        log.info("loginMember={}", loginMember);
         if (loginMember == null) {
-            return "false";
+            return ResponseEntity.notFound().build();
         }
 
-        return "true";
+        Map<String, Object> result = new HashMap<>();
+        result.put("mem_idx", loginMember.getId());
+        result.put("mem_email", loginMember.getEmail());
+        result.put("mem_nickname", loginMember.getNickname());
+
+        return new ResponseEntity<>(result,  HttpStatus.OK);
     }
 }
