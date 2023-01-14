0955965f1390094b231aecbdabb9ebee0b59f2d7
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2023-01-14 21:14:01 +0900

    Feature: 회원 로그인 함수 생성

---
+++ src/main/java/com/umc/i/member/MemberService.java
@@ -0,0 +1,17 @@
+package com.umc.i.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final JdbcTemplateMemberRepository memberRepository;
+
+    public Member login(String loginEmail, String password) {
+        return memberRepository.findByLoginEmail(loginEmail)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
