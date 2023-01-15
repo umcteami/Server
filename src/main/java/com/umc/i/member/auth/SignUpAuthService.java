a483214f2f9eb9fccab693b2d0fe4ab172f99333
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2023-01-16 06:11:40 +0900

    Feature: 인증번호 확인 레포 구현

--- src/main/java/com/umc/i/member/auth/SignUpAuthService.java
+++ src/main/java/com/umc/i/member/auth/SignUpAuthService.java
@@ -1,2 +1,36 @@
-package com.umc.i.member.auth;public class SignUpAuthSerivce {
+package com.umc.i.member.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SignUpAuthService {
+
+    private final JdbcTemplateSignUpRepository signUpRepository;
+
+    public SignAuthNumber getSignAuthNumberObject(int authIdx) {
+        return signUpRepository.findByAuthIdx(authIdx)
+                .filter(o -> o.getAuthIdx() == authIdx)
+                .orElse(null);
+    }
+
+    public boolean isEqual(SignAuthNumber signAuthNumber, int maKey) {
+        int key = signAuthNumber.getKey();
+
+        return key == maKey;
+    }
+
+    public boolean isExpired(SignAuthNumber signAuthNumber) {
+        Date createdAt = signAuthNumber.getCreatedAt();
+        Date date = new Date();
+
+        long diffSec = (date.getTime() - createdAt.getTime()) / (1000);
+        log.info("sec={}", diffSec);
+        return diffSec <= 600;
+    }
 }
