05df29e7c449e24c635b8418dab00cd1376b5873
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2023-01-16 06:13:29 +0900

    Feature: jwt 자동로그인, jwt 로그인 및 jwt 발급 구현

--- src/main/java/com/umc/i/member/login/MemberController.java
+++ src/main/java/com/umc/i/member/login/MemberController.java
@@ -1,5 +1,7 @@
 package com.umc.i.member.login;
 
+import com.umc.i.member.jwt.JwtService;
+import com.umc.i.member.login.model.PostLoginRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -8,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Slf4j
@@ -19,20 +19,52 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody Member member) {
+    private final JwtService jwtService;
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<PostLoginRes> login(@RequestHeader("Auth-Refresh") String existRefreshToken,
+                                              @RequestBody Member member) {
+        /**
+         * jwt refreshToken 이 전달된 경우 확인
+         * jwtService isValid, isExpire 후에 모두 true면
+         * uid, email 추출해서 밑에 새로운 토큰 발행 후 전달
+         */
+//        String existRefreshToken = header.get("Auth-Refresh");
+        log.info("existRefreshToken={} {}", existRefreshToken, existRefreshToken == null);
+        if (existRefreshToken.length() > 0) {
+            if (!(jwtService.isExpiredToken(existRefreshToken) && jwtService.isValidToken(existRefreshToken))) {
+                PostLoginRes res = new PostLoginRes(false);
+                return new ResponseEntity<>(res, HttpStatus.UNAUTHORIZED);
+            }
+            else {
+                PostLoginRes res = new PostLoginRes();
+                Member loginMember = new Member();
+                res.setId(Long.valueOf(jwtService.getMemberId(existRefreshToken)));
+                res.setEmail(jwtService.getMemberEmail(existRefreshToken));
+                loginMember.setEmail(res.getEmail());
+                loginMember.setId(res.getId());
+                res.setAccessToken(jwtService.createAccessToken(loginMember));
+                res.setRefreshToken(jwtService.createRefreshToken(loginMember));
+                res.setSuccess(true);
+                jwtService.updateRefreshToken(String.valueOf(loginMember.getId()), res.getRefreshToken());
+                return new ResponseEntity<>(res, HttpStatus.OK);
+            }
+        }
+
         Member loginMember = memberService.login(member.getEmail(), member.getPassword());
 
         if (loginMember == null) {
             return ResponseEntity.notFound().build();
         }
 
-        Map<String, Object> result = new HashMap<>();
-        result.put("mem_idx", loginMember.getId());
-        result.put("mem_email", loginMember.getEmail());
-        result.put("mem_nickname", loginMember.getNickname());
+        String accessToken = jwtService.createAccessToken(loginMember);
+        String refreshToken = jwtService.createRefreshToken(loginMember);
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        jwtService.updateRefreshToken(String.valueOf(loginMember.getId()), refreshToken);
+
+        PostLoginRes res = new PostLoginRes(loginMember.getId(), loginMember.getEmail(), loginMember.getNickname(), accessToken, refreshToken, true);
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     @PostMapping("/logout")
