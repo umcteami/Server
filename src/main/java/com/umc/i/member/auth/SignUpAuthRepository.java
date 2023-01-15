9827717a07d3aecd5f2d417d9092a3e94635aa17
DESKTOP-41DHC5U\HyeonJae <uc07102hi@gmail.com>
2023-01-16 06:11:16 +0900

    Feature: 인증번호 확인 레포 인터페이스

--- src/main/java/com/umc/i/member/auth/SignUpAuthRepository.java
+++ src/main/java/com/umc/i/member/auth/SignUpAuthRepository.java
@@ -1,2 +1,8 @@
-package com.umc.i.member.auth;public interface SignUpAuthRespository {
+package com.umc.i.member.auth;
+
+import java.util.Optional;
+
+public interface SignUpAuthRepository {
+
+    Optional<SignAuthNumber> findByAuthIdx(int key);
 }
