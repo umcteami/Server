a0bf567dceae27cbb536273ab14b8a7c422cd34d
Sasha <=>
2023-01-26 22:31:04 +0900

    test1

--- src/main/java/com/umc/i/IApplication.java
+++ src/main/java/com/umc/i/IApplication.java
@@ -2,6 +2,8 @@ package com.umc.i;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class IApplication {
@@ -12,6 +14,10 @@ public class IApplication {
 		// 메모리 사용량 출력
         // long heapSize = Runtime.getRuntime().totalMemory();
         // System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
+	}
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter(){
+		return new ServerEndpointExporter();
 	}
 
 }
---
+++ src/main/java/com/umc/i/src/chat/ChatConfig.java
@@ -0,0 +1,15 @@
+package com.umc.i.src.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/").setAllowedOrigins("*").withSockJS();
+    }
+}
