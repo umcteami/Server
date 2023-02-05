39c0c4c5bd199216cb173d0ec0bced0eb3f0166f
LAPTOP-8FKT8S9V\hyeon <uc07102hi@gmail.com>
2023-02-06 05:07:43 +0900

    Feature: 나눔장터 검색 init

--- src/main/java/com/umc/i/src/search/SearchController.java
+++ src/main/java/com/umc/i/src/search/SearchController.java
@@ -1,4 +1,56 @@
 package com.umc.i.src.search;
 
+import com.umc.i.config.BaseResponse;
+import com.umc.i.config.BaseResponseStatus;
+import com.umc.i.config.Constant;
+import com.umc.i.src.market.feed.model.GetMarketFeedReq;
+import com.umc.i.src.market.feed.model.GetMarketFeedRes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
 public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping("market/search")
+    public BaseResponse searchMarketFeedByKeyword(@RequestParam String category,
+                                                  @RequestParam String search_keyword,
+                                                  @RequestBody GetMarketFeedReq req) {
+
+        if (!isCategoryValid(category)) {
+            return new BaseResponse<>(BaseResponseStatus.MARKET_FEED_BY_CATEGORY_FAILED);
+        }
+        if (!isSearchKeywordValid(search_keyword)) {
+            return new BaseResponse<>(BaseResponseStatus.SEARCH_KEYWORD_NULL_EXCEPTION);
+        }
+
+        int userIdx = req.getUserIdx();
+
+        if (category == null) {
+            List<GetMarketFeedRes> feedRes = searchService.searchAllMarketFeedByKeyword(userIdx, search_keyword);
+            return new BaseResponse(feedRes);
+        } else {
+            String categoryIdx = Constant.MARKET_GOOD_CATEGORIES.get(category);
+            List<GetMarketFeedRes> feedRes = searchService.searchCategoryMarketFeedByKeyword(userIdx, categoryIdx, search_keyword);
+            return new BaseResponse(feedRes);
+        }
+    }
+
+    private boolean isCategoryValid(String category) {
+        return Constant.MARKET_GOOD_CATEGORIES.containsKey(category);
+    }
+
+    private boolean isSearchKeywordValid(String search_keyword) {
+        return search_keyword != null;
+    }
 }
+
--- src/main/java/com/umc/i/src/search/SearchService.java
+++ src/main/java/com/umc/i/src/search/SearchService.java
@@ -1,2 +1,13 @@
-package com.umc.i.src.search;public class SearchService {
+package com.umc.i.src.search;
+
+
+import com.umc.i.src.market.feed.model.GetMarketFeedRes;
+
+import java.util.List;
+
+public interface SearchService {
+
+    List<GetMarketFeedRes> searchAllMarketFeedByKeyword(int userIdx, String search_keyword);
+
+    List<GetMarketFeedRes> searchCategoryMarketFeedByKeyword(int userIdx, String categoryIdx, String search_keyword);
 }
