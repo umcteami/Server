95969e155b14e948479e8a04041959af7e2d4959
Hyobeen-Park <amanda0109@naver.com>
2023-02-05 17:17:11 +0900

    [효니] 장터후기 하나 조회 - 완료

--- src/main/java/com/umc/i/src/review/ReivewProvider.java
+++ src/main/java/com/umc/i/src/review/ReivewProvider.java
@@ -1,11 +1,16 @@
 package com.umc.i.src.review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.umc.i.config.BaseException;
 import com.umc.i.config.BaseResponseStatus;
 import com.umc.i.src.review.model.Review;
+import com.umc.i.src.review.model.get.GetReviewRes;
+import com.umc.i.utils.S3Storage.Image;
 import com.umc.i.utils.S3Storage.UploadImageS3;
 
 import lombok.AllArgsConstructor;
@@ -20,9 +25,17 @@ public class ReivewProvider {
 
 
     // 장터 후기 하나 조회
-    public Review getReview(int reviewIdx) throws BaseException {
+    public GetReviewRes getReview(int reviewIdx) throws BaseException {
         try {
-            return reviewDao.getReview(reviewIdx);
+            List<Image> img = reviewDao.getReviewsImage(reviewIdx);
+            List<String> filePath = new ArrayList();
+            if(img != null) {   // 이미지가 있으면
+                for(int i = 0; i < img.size(); i++) {
+                    filePath.add(uploadImageS3.getS3(img.get(i).getUploadFilePath()));
+                }
+            }
+            
+            return new GetReviewRes(reviewDao.getReview(reviewIdx), filePath);
         } catch (Exception e) {
             e.getStackTrace();
             throw new BaseException(BaseResponseStatus.GET_REVIEW_FAIL);
--- src/main/java/com/umc/i/src/review/model/get/GetReviewRes.java
+++ src/main/java/com/umc/i/src/review/model/get/GetReviewRes.java
@@ -1,6 +1,9 @@
 package com.umc.i.src.review.model.get;
 
 import java.util.Date;
+import java.util.List;
+
+import com.umc.i.src.review.model.Review;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,15 +16,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetReviewRes {
-    private int reviewIdx;      // 장터 후기 인덱스
-    private int buyerIdx;       // 구매자 인덱스
-    private int sellerIdx;          // 판매자 인덱스
-    private String buyerNick;       // 구매자 닉네임
-    private String sellerNick;      // 판매자 닉네임
-    private String goods;       // 구매 물품
-    private String content;     // 내용
-    private int hit;        // 조회수
-    private Date createAt;      // 작성일
+    private Review review;  // 장터 후기
+    private List<String> img;   // 이미지
 }
 
 
